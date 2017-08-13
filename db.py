7ecc1317b2f075ce74b7fa427e33f6cda3657f0e
Lou <lou@Lous-iMac.home>
2017-08-13 08:30:16 -0400

    rm

--- db.py
+++
@@ -1,8 +0,0 @@
-from application.users.models import User
-new_user = User(email='lou2@me.com', username='me2')
-
-from application import db
-db.session.add(new_user)
-db.session.commit()
-
-print new_user.email
